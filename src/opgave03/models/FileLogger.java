c931d637e4d84d7b72fdb169e60174511426fea9
August Juel Aagaard <augustjaagaard@gmail.com>
2024-04-15 12:39:07 +0200

    Can now append to file

--- src/opgave03/models/FileLogger.java
+++ src/opgave03/models/FileLogger.java
@@ -1,18 +1,20 @@
 package opgave03.models;
 
+import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class FileLogger implements LogType {
-    String fileLocation;
+    private String fileLocation;
+
     public FileLogger(String fileLocation) {
         this.fileLocation = fileLocation;
     }
 
     @Override
     public void logInfo(String message) {
-        try (PrintWriter printWriter = new PrintWriter(this.fileLocation)) {
+        try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(fileLocation, true))) {
             String logMessage = "[" + LocalDate.now() + "][" + LocalTime.now() + "][INFO] " + message;
             printWriter.println(logMessage);
         } catch (Exception e) {
@@ -22,7 +24,7 @@ public class FileLogger implements LogType {
 
     @Override
     public void logError(String message) {
-        try (PrintWriter printWriter = new PrintWriter(this.fileLocation)) {
+        try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(fileLocation, true))) {
             String logMessage = "[" + LocalDate.now() + "][" + LocalTime.now() + "][ERROR] " + message;
             printWriter.println(logMessage);
         } catch (Exception e) {
